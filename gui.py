94984e9a8927a74331321a6aae4e1337a3146dab
Eric Sullivan <eric.sullivan@maine.edu>
2020-06-03 13:10:16 -0400

    Can graph both US and WorldWide cases - Thinking about changing how it downloads and saves data

--- gui.py
+++ gui.py
@@ -17,8 +17,20 @@ class App(tk.Frame):
         self.usDeaths.pack(side = "top")
 
         self.usCases = tk.Button(self, text = "US Confirmed Covid Cases")
-        self.usCases["command"] = self.plotAllConfrimedUS
+        self.usCases["command"] = self.plotAllConfirmedUS
         self.usCases.pack(side = "top")
+
+        self.worldDeaths = tk.Button(self, text = "World Covid Deaths")
+        self.worldDeaths["command"] = self.plotAllDeathsWD
+        self.worldDeaths.pack(side = "top")
+
+        self.worldConfirmed = tk.Button(self, text = "World Confirmed Covid Cases")
+        self.worldConfirmed["command"] = self.plotAllConfirmedWD
+        self.worldConfirmed.pack(side = "top")
+
+        self.worldRecoverd = tk.Button(self, text = "World Recovered Covid Cases")
+        self.worldRecoverd["command"] = self.plotAllRecoveredWD
+        self.worldRecoverd.pack(side = "top") 
 
         self.quit = tk.Button(self, text = "Quit", fg = "red", command = self.master.destroy)
         self.quit.pack(side = "bottom")
@@ -26,11 +38,27 @@ class App(tk.Frame):
     def plotAllDeathsUS(self):
         deaths = CovidData('https://tinyurl.com/vxbdvgo')
         deaths.plotData("Total Deaths in the US", 'r-')
+        del(deaths)
 
-    def plotAllConfrimedUS(self):
+    def plotAllConfirmedUS(self):
         confirmed = CovidData('https://tinyurl.com/uynhaxd')
         confirmed.plotData("Total Confrimed Covid Cases in the US", 'b-')
+        del(confirmed)
+    
+    def plotAllDeathsWD(self):
+        deaths = CovidData('https://tinyurl.com/rlssflz')
+        deaths.plotData("Total Deaths Globally", 'r-')
+        del(deaths)
 
+    def plotAllConfirmedWD(self):
+        confirmed = CovidData('https://tinyurl.com/tsqkf7y')
+        confirmed.plotData("Total Confirmed Covid Cases WorldWide", 'b-')
+        del(confirmed)
+
+    def plotAllRecoveredWD(self):
+        recovered = CovidData('https://tinyurl.com/vtcebxt')
+        recovered.plotData("Total Recovered Cases WorldWide", 'g-')
+        del(recovered)
 
 root = tk.Tk()
 app = App(master = root)
